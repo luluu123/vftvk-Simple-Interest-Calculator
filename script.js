eac90fd0ad2558004033d6939f42577d7f3baa37
luluu123 <88992329+luluu123@users.noreply.github.com>
2021-08-21 19:59:29 -0300

    yes

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    r = document.getElementById("rate").value;
-    y = document.getElementById("years").value;
+function Compute() {
+  var p = document.getElementById("principal").value;
+  var r = document.getElementById("rate").value;
+  var y = document.getElementById("years").value;
+  var year = new Date().getFullYear();
+  console.log("XD");
 
-    i = p*((r/100)*y);
-    document.getElementById("result").innerHTML = "<br/>" + "If you deposit " + p + ",<br />" + 
-    "at an interest rate of " + r + ".<br />" +
-    "You will receive an amount of " + i.toFixed(0) + ",<br />" +
-    "in the year " + y + " <br />";
-    //console.log(i);
+  var i = p * ((r / 100) * y);
+  document.getElementById("result").innerHTML =
+    "If you deposit " +
+    p +
+    ",<br />" +
+    "at an interest rate of " +
+    r +
+    ".<br />" +
+    "You will receive an amount of " +
+    i.toFixed(0) +
+    ",<br />" +
+    "in the year " +
+    year +
+    " <br />";
 }
-
